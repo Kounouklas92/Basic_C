f63b7e9b89180f5d91e85b6f96a55b6e3a1e6b24
Kounouklas92 <thkoun92@gmail.com>
2021-12-27 19:05:13 +0200

    Add files

---
+++ Structs_examples.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct
+{
+	uint8_t price;
+	uint8_t colour;
+	unit8_t *random;		
+}car;
+
+int main()
+{
+	
+	
+	
+	
+	
+	
+	
+	
+return 0;	
+	
+}
