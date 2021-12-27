759b7890d11362e6392e7326345de6b3a0804820
Kounouklas92 <thkoun92@gmail.com>
2021-12-27 21:36:14 +0200

    struct member access

--- Structs_examples.c
+++ Structs_examples.c
@@ -4,20 +4,34 @@
 typedef struct
 {
 	uint8_t price;
-	uint8_t colour;
-	unit8_t *random;		
-}car;
+	uint8_t age;
+    uint8_t* random;		
+}__attribute__((packed)) car;
+
+
+car car1 = {230,25,122};
+car* pcar;
 
 int main()
 {
+	printing_results();
 	
-	
-	
-	
-	
-	
-	
-	
-return 0;	
-	
+	printf("%d\n",car1.random);
+	pcar = &car1;
+	printf("%d\n",pcar->random);
+		
+	return 0;	
 }
+
+void printing_results(void)
+{
+	printf("size car struct is:       %d ",sizeof(car1));
+	printf("address car struct is:    %p\n",&car1);
+	printf("size of price member is:  %d  ",sizeof(car1.price));
+	printf("address price member is:  %p\n",&car1.price);
+	printf("size of age member is:    %d  ",sizeof(car1.age));
+	printf("address age member is:    %p\n",&car1.age);
+	printf("size of random member is: %d  ",sizeof(car1.random));
+	printf("address random member is: %p\n",&car1.random);
+}
+
