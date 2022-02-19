fdc54cd35b6d32232f6cb96449d4c755d31ac4fd
DESKTOP-7VMC9NV\Thanasis Kounouklas <thkoun92@gmail.com>
2022-02-19 19:57:43 +0200

    structure members

--- Structs_examples.c
+++ Structs_examples.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+void printing_results(void);
 
 typedef struct
 {
 	uint8_t price;
 	uint8_t age;
-    uint8_t* random;		
+    uint8_t* random;
+	uint8_t driverAge;		
 }__attribute__((packed)) car;
 
 
-car car1 = {230,25,122};
+car car1 = {230,25};
 car* pcar;
 
 int main()
 {
 	printing_results();
+	//Allocate memory for integer
+    car1.random =  malloc(sizeof(uint8_t*));
+    //check allocated memory
+    if((car1.random) == NULL)
+    {
+      //  printf("FAIL TO ALLOCATE MEMORY\n");
+    }
+    // Copy 12 in piData
+    *car1.random = 122;
 	
-	printf("%d\n",car1.random);
+	printf("%u\n",*car1.random);
 	pcar = &car1;
-	printf("%d\n",pcar->random);
+	printf("%u\n",*pcar->random);
 		
 	return 0;	
 }
 
 void printing_results(void)
 {
-	printf("size car struct is:       %d ",sizeof(car1));
+	printf("size of car struct is:    %d ",sizeof(car1));
 	printf("address car struct is:    %p\n",&car1);
 	printf("size of price member is:  %d  ",sizeof(car1.price));
 	printf("address price member is:  %p\n",&car1.price);
@@ -33,5 +46,7 @@ void printing_results(void)
 	printf("address age member is:    %p\n",&car1.age);
 	printf("size of random member is: %d  ",sizeof(car1.random));
 	printf("address random member is: %p\n",&car1.random);
+	printf("size of driverAge member is: %d  ",sizeof(car1.driverAge));
+	printf("address driverAge member is: %p\n",&car1.driverAge);
 }
 
